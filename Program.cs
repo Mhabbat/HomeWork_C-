bddb864f5230952dccbf557ea5884f95f5a412af
unknown <ma-xa-bat@mail.ru>
2022-12-19 21:57:58 +0300

    Home work

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 2.Напишите программу,которая на вход принимает два числа и выдает,какое число больше,а какое меньше.
+/*int a = 17;
+int b = 38;
+int max = a;
+int min = a;
+{
+if(b > max) max = b;
+if(b < min) min = b;
+}
+Console.WriteLine( max);
+Console.WriteLine( min);
+*/
+
+/*Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+int min = num1;
+if(num2 > max) max = num2;
+if(num2 < min) min = num2;
+
+Console.WriteLine(max);
+Console.WriteLine(min);
+*/
+
+// Задача 4. Напишите программу,которая на вход принимает три числа и выдает максимальное из этих чисел.
+
+
+/*
+int a = 38;
+int b = 92;
+int c = 76;
+int max = a;
+{
+    if(b > max) max = b;
+    if(c > max) max = c;
+}
+Console.WriteLine (max);
+*/
+/*Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+
+Console.WriteLine(max);
+*/
+// Задача 6.Напишите программу,которая на вход принимает число и выдает,является ли число четным (делится ли оно на два без остатка).
+
+
+/*
+Console.WriteLine("Input a number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+if (num % 2==0)
+{
+Console.WriteLine("even number");
+}
+else
+{
+    Console.WriteLine("odd number");
+} */
+// Напишите программу,которая на вход принимает число (N),а на выходе показывает все четные число.
+
+
+
+
+
+// Задача 8.Напишите программу,на вход принимает число (N),а на выходе показывает все четные числа от 1 до N.
+/*
+Console.WriteLine("Все четное число до N");
+Console.Write("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int x = N -1; x >=2; x--)
+if ( x % 2 == 0)
+Console.WriteLine(x); */
+
+
+
+
+
+
+
