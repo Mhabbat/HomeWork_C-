024f3dfcdc9239413d0bd667ca9e5ba16c55f519
Mhabbat <119960455+Mhabbat@users.noreply.github.com>
2022-12-23 22:11:26 +0300

    HomeWork2

--- Program.cs
+++ Program.cs
@@ -1,87 +1,64 @@
-﻿// Задача 2.Напишите программу,которая на вход принимает два числа и выдает,какое число больше,а какое меньше.
-/*int a = 17;
-int b = 38;
-int max = a;
-int min = a;
-{
-if(b > max) max = b;
-if(b < min) min = b;
-}
-Console.WriteLine( max);
-Console.WriteLine( min);
-*/
-
-/*Console.Write("Input a first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-int max = num1;
-int min = num1;
-if(num2 > max) max = num2;
-if(num2 < min) min = num2;
-
-Console.WriteLine(max);
-Console.WriteLine(min);
-*/
-
-// Задача 4. Напишите программу,которая на вход принимает три числа и выдает максимальное из этих чисел.
-
+﻿/* 
+Урок 2. Базовые алгоритмы
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.*/
+ 
 
 /*
-int a = 38;
-int b = 92;
-int c = 76;
-int max = a;
+Console.WriteLine("Введите трехзначное число:");
+int number=Convert.ToInt32(Console.ReadLine());
+int x=10;
+int result = number / x;
+Console.WriteLine(result % x);
+*/
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+/*
+
+int NumThird(int num)
 {
-    if(b > max) max = b;
-    if(c > max) max = c;
+    while(num > 1000)
+    {
+        num = num /10;
+    }
+    num =num % 10;
+    return num;
 }
-Console.WriteLine (max);
+Console.Write("Введите число:");
+int num2 =
+Convert.ToInt32(Console.ReadLine());
+if (num2<100)
+{
+    Console.WriteLine("третьей цифра нет");
+}
+else
+Console.WriteLine("Третья цифра этого числа:" + NumThird(num2));
 */
-/*Console.Write("Input a first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a third number");
-int num3 = Convert.ToInt32(Console.ReadLine());
-int max = num1;
-if(num2 > max) max = num2;
-if(num3 > max) max = num3;
 
-Console.WriteLine(max);
-*/
-// Задача 6.Напишите программу,которая на вход принимает число и выдает,является ли число четным (делится ли оно на два без остатка).
 
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 /*
-Console.WriteLine("Input a number: ");
-int num=Convert.ToInt32(Console.ReadLine());
-if (num % 2==0)
+Console.WriteLine("Введите число неделя");
+int number=Convert.ToInt32(Console.ReadLine());
+int a=6;
+int b=7;
+
+if(number ==a)
 {
-Console.WriteLine("even number");
+    Console.WriteLine("Этот день выходной.");
+}
+if(number ==b)
+{
+    Console.WriteLine("Этот день выходной.");
 }
 else
 {
-    Console.WriteLine("odd number");
-} */
-// Напишите программу,которая на вход принимает число (N),а на выходе показывает все четные число.
-
-
-
-
-
-// Задача 8.Напишите программу,на вход принимает число (N),а на выходе показывает все четные числа от 1 до N.
-/*
-Console.WriteLine("Все четное число до N");
-Console.Write("Введите число");
-int N = Convert.ToInt32(Console.ReadLine());
-for (int x = N -1; x >=2; x--)
-if ( x % 2 == 0)
-Console.WriteLine(x); */
-
-
-
-
-
-
-
+ Console.WriteLine("Это будни день");
+}
+*/
