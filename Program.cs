aa3f64f9546da6dd9bf0853d33eb96855599648c
Mhabbat <119960455+Mhabbat@users.noreply.github.com>
2022-12-26 23:39:34 +0300

    HomeWork 3

--- Program.cs
+++ Program.cs
@@ -1,64 +1,77 @@
-﻿/* 
-Урок 2. Базовые алгоритмы
-Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.*/
- 
+﻿//Задача 19
 
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 /*
-Console.WriteLine("Введите трехзначное число:");
-int number=Convert.ToInt32(Console.ReadLine());
-int x=10;
-int result = number / x;
-Console.WriteLine(result % x);
-*/
-
-/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-645 -> 5
-
-78 -> третьей цифры нет
-
-32679 -> 6 */
-/*
-
-int NumThird(int num)
+bool Palindrom(int n)
 {
-    while(num > 1000)
+int x=0,y=n;
+int a =10;
+while(n > 0)
+
     {
-        num = num /10;
+        x=x * a + n % a;
+        n=n / a;
     }
-    num =num % 10;
-    return num;
+return x == y;
 }
+int n;
+
 Console.Write("Введите число:");
-int num2 =
-Convert.ToInt32(Console.ReadLine());
-if (num2<100)
-{
-    Console.WriteLine("третьей цифра нет");
-}
+n = Convert.ToInt32(Console.ReadLine());
+if (Palindrom(n))
+Console.WriteLine("Палиндром");
 else
-Console.WriteLine("Третья цифра этого числа:" + NumThird(num2));
+Console.WriteLine("Не палиндром");
+/*
+
+
+/*Задача 21
+
+/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+
+double Distanse (double x1,double y1,double z1,
+                   double x2,double y2,double z2)
+{
+double res1 = Math.Pow(x2 - x1, 2);
+double res2 = Math.Pow(y2 - y1, 2);
+double res3 = Math.Pow(z2 - z1, 2);
+double result = Math.Sqrt(res1+res2+res3);
+return result;
+}
+Console.WriteLine("Введите кординаты первой точек:");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите кординаты второй точек:");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Расстояние между точками:" + Distanse(x1, y1,z1,x2,y2,z2));Console.Write("Введите кординаты первой точек:");
 */
 
+                 
+/*Задача 23
 
-//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+*/
 
 /*
-Console.WriteLine("Введите число неделя");
-int number=Convert.ToInt32(Console.ReadLine());
-int a=6;
-int b=7;
+void Cube (int n)
+{
+    int count =1;
+    Console.WriteLine("Кубы чисел");
+    while(count < n+1)
+    {
+        Console.Write(count*count*count +" ");
+        count++;
+    }
 
-if(number ==a)
-{
-    Console.WriteLine("Этот день выходной.");
 }
-if(number ==b)
-{
-    Console.WriteLine("Этот день выходной.");
-}
-else
-{
- Console.WriteLine("Это будни день");
-}
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+Cube (n);
 */
