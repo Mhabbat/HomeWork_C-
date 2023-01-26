dd7adeb4bbab24c5c963f253358db2b0dffb4df4
Mhabbat <119960455+Mhabbat@users.noreply.github.com>
2023-01-26 09:00:15 +0300

    Add files via upload
    HomeWork_6

--- Program.cs
+++ Program.cs
@@ -1,77 +1,89 @@
-﻿
-//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 3, 5 -> 243 (3⁵)
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
 /*
-void NumDegree (int a,int b)
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
 {
-    int result = 1;
-    for(int i = 1; i<=b; i++)
+    if (numbers[i] > 0)
     {
-        result *= a;
+        sum++;
     }
-Console.WriteLine($"{a} extent  {b} = {result}");
 }
-Console.Write("Input a number:a ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a number:b ");
-int numberB=Convert.ToInt32(Console.ReadLine());
- NumDegree(numberA,numberB);
-
-/*   Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-
-Console.Write("Input a number: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-
-int SumNumber (int numberA )
-{
-    int result = 0;
-    while(numberA > 0)
-    {
-        result+= numberA % 10;
-        numberA =numberA / 10; 
-    }
-    return result;
-} 
-
-  int sumNumber = SumNumber(numberA);
-Console.WriteLine("Сумма цифр в числе: " + sumNumber);
-
-
-   
-/*Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
-
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] */
-
-/*
-  int [] CreateArray(int size)
-{
-    int[] array = new int [size];
-    for(int i = 0; i < size; i++)
-    {
-       Console.Write($"Введите {i + 1} элемент: ");
-       array[i] = Convert.ToInt32(Console.ReadLine()); 
-    }
-      
-
-      return array;  
-}
-
-void ShowArray(int[] array)
-
-
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-    Console.Write(array[i] + " ");
-  
 Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
     }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
 }
 
-Console.Write("Введите длина массива: ");
-int length = Convert.ToInt32(Console.ReadLine());
 
-    
-int [] newArray = CreateArray (length);
-ShowArray(newArray);
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}*/
+
+/* Напишите программу, которая найдёт точку пересечения двух прямых, 
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения 
+ b1, k1, b2 и k2 задаются пользователем.
+
+ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 */
